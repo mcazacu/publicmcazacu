f1f5399e5ecf27dfe8dc41a2e1e13d93c0d95d70
Marku <Marku@DESKTOP-PUACQTD>
2020-06-13 14:29:35 +0200

    Initial import

---
+++ calc_starter_uebung/src/main/java/hftstuttgart/Calculator.java
@@ -0,0 +1,18 @@
+package hftstuttgart;
+
+
+public class Calculator {
+    
+    public int sum(int op1, int op2) {
+        int sum = 0;
+        sum += op1;
+        sum += op2;
+        return sum;
+    }    
+
+    public int prod(int op1, int op2) {
+        int sum = 0;
+        sum = op1*op2;
+        return sum;
+    }    
+}
